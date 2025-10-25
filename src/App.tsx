bdca73011a350a4db6161a3d0fd394cd7db50123
AdnenKh <adnenkhiari484@gmail.com>
2025-10-25 15:57:48 +0100

    added blur effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import SplashCursor from './components/FluidCursor'
+import GradualBlur from "./components/GradualBlur";
 
 function App() {
   return (
@@ -11,6 +12,16 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <GradualBlur
+        target="page"
+        position="bottom"
+        height="6rem"
+        strength={2}
+        divCount={5}
+        curve="bezier"
+        exponential={true}
+        opacity={1}
+      />
       </>
     </Suspense>
   );
