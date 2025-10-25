4253b28b62291f3f481393503357687df1f3ce9d
Name <adnenkhiari484@gmail.com>
2025-10-25 15:16:56 +0000

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home";
+import CurriculumDetailPage from "./pages/CurriculumDetailPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import SplashCursor from './components/FluidCursor'
 import GradualBlur from "./components/GradualBlur";
 
@@ -8,20 +11,23 @@ function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <>
-      <SplashCursor />
+        <SplashCursor />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/curriculum/:id" element={<CurriculumDetailPage />} />
         </Routes>
+        <Footer />
         <GradualBlur
-        target="page"
-        position="bottom"
-        height="6rem"
-        strength={2}
-        divCount={5}
-        curve="bezier"
-        exponential={true}
-        opacity={1}
-      />
+          target="page"
+          position="bottom"
+          height="6rem"
+          strength={2}
+          divCount={5}
+          curve="bezier"
+          exponential={true}
+          opacity={1}
+        />
       </>
     </Suspense>
   );
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,3 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
 import Hero from "./Hero";
 import AboutSection from "./AboutSection";
 import CurriculumList from "./CurriculumList";
@@ -9,12 +7,10 @@ import { Toaster } from "./ui/toaster";
 function Home() {
   return (
     <div className="w-full min-h-screen bg-[#0a0a1f]">
-      <Navbar />
       <Hero />
       <AboutSection />
       <CurriculumList />
       <ContactForm />
-      <Footer />
       <Toaster />
     </div>
   );
